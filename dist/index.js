d250b0854987ed9f954d67682935b2f441861409
Charonyuu <bensona1219@gmail.com>
2024-04-15 12:19:04 +0800

    add build

--- dist/index.js
+++ dist/index.js
@@ -31,8 +31,17 @@ const core = __importStar(require("@actions/core"));
 const utils_1 = require("./utils");
 async function ithomeAction() {
     try {
-        const browser = await puppeteer_1.default.launch({ headless: false });
+        const browser = await puppeteer_1.default.launch({
+            headless: true,
+            args: ["--no-sandbox"],
+            env: {
+                DISPLAY: ":10.0",
+            },
+        });
+        console.log("start puppeteer...");
         const page = await browser.newPage();
+        await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36");
+        await page.setJavaScriptEnabled(true);
         //   const token = core.getInput("token");
         let limit = parseInt(core.getInput("limit")); // 從輸入參數中取得 limit
         if (isNaN(limit)) {
@@ -49,10 +58,13 @@ async function ithomeAction() {
         }
         let userId = core.getInput("userId"); // 從輸入參數中取得 userId
         if (!userId) {
-            userId = "20162289";
-            // throw new Error("User ID is required.");
+            throw new Error("User ID is required.");
         }
-        await page.goto(`https://ithelp.ithome.com.tw/users/${userId}/articles`);
+        console.log("go to page...");
+        await page.goto(`https://ithelp.ithome.com.tw/users/${userId}/articles`, {
+            waitUntil: "networkidle2",
+        });
+        console.log("getting data...");
         await page.waitForSelector(".qa-list");
         const result = await page.evaluate(() => {
             const list = document.querySelectorAll(".qa-list");
@@ -83,8 +95,10 @@ async function ithomeAction() {
             });
             return articleList;
         });
+        console.log("finish getting data..., closing browser...");
         console.log(result);
         await browser.close();
+        console.log("closed browser...");
         const markdownContent = result
             .splice(0, limit)
             .map((article) => `- [${article.title}](${article.url}) - Likes: ${article.like}, Comments: ${article.comment}, Views: ${article.view}`)
